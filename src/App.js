894e21ab45add4833a9c26ef53797a894a9bbcba
vietnguyen99 <nguyenducviet888999@gmail.com>
2023-09-02 23:09:11 +0700

    Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publcRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 function App() {
     return (
-        <div className="App">
-            <h1>Nguyen duc viet</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publcRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
