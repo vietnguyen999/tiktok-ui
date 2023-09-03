2e0ec8ebe60fb4201412387a7d651931842651aa
vietnguyen99 <nguyenducviet888999@gmail.com>
2023-09-03 14:00:40 +0700

    Xây dựng UI phần Header Search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import classNames from 'classnames/bind'
-import styles from './Header.module.scss'
-
-const cx= classNames.bind(styles)
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleXmark,
+    faMagnifyingGlass,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
 function Header() {
-  return <header className={cx('wrapper')}>
-    <div className={cx('inner')}>
-
-    </div>
-  </header>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                    />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon
+                        className={cx('loading')}
+                        icon={faSpinner}
+                    />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+                  login
+                </div>
+            </div>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
